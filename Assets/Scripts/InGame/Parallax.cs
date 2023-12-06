8a1a892c722fc5642e564d4ff84ee8b39d028765
Chosungwoo123 <ejyoo12345@naver.com>
2023-12-07 00:37:16 +0900

    Make a Background Parallax.

---
+++ Assets/Scripts/InGame/Parallax.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public float parallaxEffect;
+    public GameObject cam;
+
+    private float length, startpos;
+
+    private void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length)
+        {
+            startpos += length;
+        }
+        else if (temp < startpos - length)
+        {
+            startpos -= length;
+        }
+    }
+}
