481023561209bba8c9c86a24bb5ad3d0698bd4ef
chosungwoo <ejyoo12345@naver.com>
2023-12-27 21:03:50 +0900

    Update Enemy Spawner.

--- Assets/Scripts/InGame/EnemySpawner.cs
+++ Assets/Scripts/InGame/EnemySpawner.cs
@@ -6,19 +6,54 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private Transform[] spawnPoints;
     [SerializeField] private float spawnTime;
-    [SerializeField] private GameObject[] enemies;
+    [SerializeField] private List<EnemyData> enemyDatas;
+
+    [System.Serializable]
+    public struct EnemyData
+    {
+        public GameObject enemyPrefab;
+        public int weight;
+    }
 
     private float timer;
+
+    private int total;
+
+    private void Start()
+    {
+        for (int i = 0; i < enemyDatas.Count; i++)
+        {
+            total += enemyDatas[i].weight;
+        }
+    }
 
     private void Update()
     {
         if (timer >= spawnTime)
         {
-            Instantiate(enemies[Random.Range(0, enemies.Length)], spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+            SpawnEnemy();
 
             timer = 0;
         }
 
         timer += Time.deltaTime;
+    }
+
+    private void SpawnEnemy()
+    {
+        int weight = 0;
+        int selectNum;
+
+        selectNum = Mathf.RoundToInt(total * Random.Range(0.0f, 1.0f));
+
+        for (int i = 0; i < enemyDatas.Count; i++)
+        {
+            weight += enemyDatas[i].weight;
+
+            if (selectNum <= weight)
+            {
+                Instantiate(enemyDatas[i].enemyPrefab, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+            }
+        }
     }
 }
