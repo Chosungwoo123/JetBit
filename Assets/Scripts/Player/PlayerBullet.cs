bd5cb7803064e806df6c380f1e1aaf3455b5ad87
Chosungwoo123 <ejyoo12345@naver.com>
2024-01-09 16:11:52 +0900

    Update Balance.

--- Assets/Scripts/Player/PlayerBullet.cs
+++ Assets/Scripts/Player/PlayerBullet.cs
@@ -22,30 +22,7 @@ public class PlayerBullet : MonoBehaviour
     {
         rigid.velocity = transform.up * moveSpeed;
 
-        StartCoroutine(ScaleRoutine());
         StartCoroutine(DieRoutine());
-    }
-
-    private IEnumerator ScaleRoutine()
-    {
-        Vector3 curScale = transform.localScale;
-
-        curScale.x = 0;
-
-        transform.localScale = curScale;
-
-        while (curScale.x < 1)
-        {
-            curScale.x += Time.deltaTime / 0.15f;
-
-            transform.localScale = curScale;
-
-            yield return null;
-        }
-
-        curScale.x = 1;
-
-        transform.localScale = curScale;
     }
 
     private IEnumerator DieRoutine()
