45c5887cdcb1800dd508e48c56a99cec141d75a3
Chosungwoo123 <ejyoo12345@naver.com>
2024-02-17 01:17:31 +0900

    Make a Screen Cursor.

---
+++ Assets/Scripts/UI/InGame/ScreenCursor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenCursor : MonoBehaviour
+{
+    private void Awake()
+    {
+        // Set hardware cursor off
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        transform.position = Input.mousePosition;
+    }
+}
