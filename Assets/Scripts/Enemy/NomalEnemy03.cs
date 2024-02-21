e78decf8e1d8239258fa9993e3afc9601b313890
Chosungwoo123 <ejyoo12345@naver.com>
2024-02-22 02:24:17 +0900

    Make a Nomal Enemy 03 Movement.

--- Assets/Scripts/Enemy/NomalEnemy03.cs
+++ Assets/Scripts/Enemy/NomalEnemy03.cs
@@ -4,5 +4,36 @@ using UnityEngine;
 
 public class NomalEnemy03 : EnemyBase
 {
+    protected override void Start()
+    {
+        base.Start();
 
+        SetRandomTargetPos();
+        InvokeRepeating(nameof(SetRandomTargetPos), 4, 4);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        ChildUpdate();
+    }
+
+    protected override void TargetUpdate()
+    {
+
+    }
+
+    private void ChildUpdate()
+    {
+        if (transform.childCount <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void SetRandomTargetPos()
+    {
+        targetPos = new Vector3(Random.Range(-96, 96), Random.Range(-17, 17), 0);
+    }
 }
